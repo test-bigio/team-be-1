4d0e74bdc01f53691f1475cbcfc605aa224cddb1
aquaswim <bayusetiawan37@gmail.com>
2023-03-22 17:27:50 +0700

    feat(leave): base logic of add request leave

--- BigioHrServices/Services/LeaveServices.cs
+++ BigioHrServices/Services/LeaveServices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BigioHrServices.Db;
+using BigioHrServices.Db.Entities;
 using BigioHrServices.Model.Datatable;
 using BigioHrServices.Model.Leave;
 
@@ -11,7 +12,8 @@ namespace BigioHrServices.Services
         void Reject(int id);
         LeaveQuotaResponse GetLeaveQuota(string id);
         DatatableResponse GetLeaveHistory(string id, LeaveHistoryRequest request);
-  }
+        void AddNewLeaveRequest(AddNewLeaveRequest request, string currentUserNik);
+    }
 
     public class LeaveService : ILeaveService
 	{
@@ -25,12 +27,28 @@ namespace BigioHrServices.Services
 
         public void Approve(int id)
         {
-            throw new NotImplementedException();
+            // todo logic melimpahkan
+            var leave = _getLeaveDataOrFail(id);
+
+            if (!leave.IsAlreadyReviewed())
+            {
+                throw new Exception("Request already reviewed");
+            }
+            
+            leave.Status = Leave.RequestStatus.Approved;
+            _db.SaveChanges();
         }
 
         public void Reject(int id)
         {
-            throw new NotImplementedException();
+            var leave = _getLeaveDataOrFail(id);
+
+            if (!leave.IsAlreadyReviewed())
+            {
+                throw new Exception("Request already reviewed");
+            }
+            leave.Status = Leave.RequestStatus.Rejected;
+            _db.SaveChanges();
         }
 
         public LeaveQuotaResponse GetLeaveQuota(string nik)
@@ -75,6 +93,59 @@ namespace BigioHrServices.Services
                 NextPage = (request.PageSize * request.Page) < leaves.Count,
                 PrevPage = request.Page > 1,
             };
+        }
+        public void AddNewLeaveRequest(AddNewLeaveRequest request, string currentUserNik)
+        {
+            // todo get this flag from position
+            var currentUserHaveHighestPosition = false;
+            if (currentUserHaveHighestPosition)
+            {
+                // todo if user have highest position then max quota permonth = 2
+            }
+            else
+            {
+                var quotaResponse = GetLeaveQuota(currentUserNik);
+                if (quotaResponse.LeaveAvailable < request.TotalLeaveInDays)
+                {
+                    throw new Exception("Insufficient leave quota");
+                }
+            }
+            // todo validasi matrik pelimpahan
+            
+            var reviewer = _getReviewerForNik(currentUserNik);
+            var leaveData = new Leave
+            {
+                StafNIK = currentUserNik,
+                DelegatedStafNIK = request.DelegatedNIK,
+                ReviewerNIK = reviewer.NIK,
+                Status = Leave.RequestStatus.InReview,
+                LeaveStart = request.LeaveStart,
+                TotalLeaveInDays = request.TotalLeaveInDays,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+            _db.Leaves.Add(leaveData);
+            _db.SaveChanges();
+            if (currentUserHaveHighestPosition)
+            {
+                // still not test this implementation
+                // Approve(leaveData.Id);
+            }
+        }
+        private Employee _getReviewerForNik(string currentUserNik)
+        {
+            // todo implement get reviewer for nik
+            return new Employee { NIK = "69123" };
+        }
+
+        private Leave _getLeaveDataOrFail(int id)
+        {
+            var leave = _db.Leaves.Find(id);
+            if (leave == null)
+            {
+                throw new Exception("Data not found");
+            }
+            return leave;
         }
     }
 }
