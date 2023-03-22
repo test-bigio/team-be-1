76a8b4df2688b2446990d466f8c9cc04d05b5885
RefauRah <reza.fauzi@radyalabs.id>
2023-03-22 23:58:12 +0700

    CHANGE: Initial Migration

---
+++ BigioHrServices/Migrations/20230322165352_InitialCreate.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BigioHrServices.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditModuls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IpAddress = table.Column<string>(type: "text", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Module = table.Column<string>(type: "text", nullable: false),
+                    Activity = table.Column<string>(type: "text", nullable: false),
+                    Detail = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditModuls", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Delegations",
+                columns: table => new
+                {
+                    nik = table.Column<string>(type: "text", nullable: false),
+                    parent_nik = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Delegations", x => x.nik);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DigitalPinLogs",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    staff_id = table.Column<string>(type: "text", nullable: false),
+                    pin = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DigitalPinLogs", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    nik = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    sex = table.Column<string>(type: "text", nullable: false),
+                    join_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    work_length = table.Column<string>(type: "text", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    digital_signature = table.Column<string>(type: "text", nullable: false),
+                    jatah_cuti = table.Column<int>(type: "integer", nullable: false),
+                    last_update_password = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    is_on_leave = table.Column<bool>(type: "boolean", nullable: false),
+                    position_id = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    otp = table.Column<string>(type: "text", nullable: true),
+                    created_by = table.Column<long>(type: "bigint", nullable: true),
+                    created_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    updated_by = table.Column<long>(type: "bigint", nullable: true),
+                    updated_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.nik);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Leaves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StafNIK = table.Column<string>(type: "text", nullable: false),
+                    DelegatedStafNIK = table.Column<string>(type: "text", nullable: false),
+                    ReviewerNIK = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    LeaveDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leaves", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    code = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    level = table.Column<string>(type: "text", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nik = table.Column<string>(type: "text", nullable: false),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    body = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    is_read = table.Column<bool>(type: "boolean", nullable: false),
+                    read_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    created_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Employees_nik",
+                        column: x => x.nik,
+                        principalTable: "Employees",
+                        principalColumn: "nik",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_nik",
+                table: "Notifications",
+                column: "nik");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditModuls");
+
+            migrationBuilder.DropTable(
+                name: "Delegations");
+
+            migrationBuilder.DropTable(
+                name: "DigitalPinLogs");
+
+            migrationBuilder.DropTable(
+                name: "Leaves");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
