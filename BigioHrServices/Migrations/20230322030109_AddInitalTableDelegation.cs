5e42b9d54d1ad2e2000fc5dde680926eeebd3118
indra.army <indra.army@skyshi.com>
2023-03-22 12:45:32 +0700

    feat(delegation): add migration

---
+++ BigioHrServices/Migrations/20230322030109_AddInitalTableDelegation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BigioHrServices.Migrations
+{
+    public partial class AddInitalTableDelegation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Delegations",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false, name: "id")
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NIK = table.Column<string>(type: "text", nullable: false, name: "nik"),
+                    ParentNIK = table.Column<string>(type: "text", nullable: false, name: "parent_nik")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Delegations", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Delegations");
+        }
+    }
+}
