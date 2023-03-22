cb0e3fb70f4e6b39da35f39b035cc9c5da7683f9
aquaswim <bayusetiawan37@gmail.com>
2023-03-22 22:37:43 +0700

    fix(leave): simplify leave data

--- BigioHrServices/Migrations/20230322153707_SimplifyLeaveData.Designer.cs
+++ BigioHrServices/Migrations/20230322153707_SimplifyLeaveData.Designer.cs
@@ -3,17 +3,19 @@ using System;
 using BigioHrServices.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace BigioHrServices.Db.Entities
+namespace BigioHrServices.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230322153707_SimplifyLeaveData")]
+    partial class SimplifyLeaveData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
