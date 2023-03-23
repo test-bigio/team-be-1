2a91eb3cb564e2673e924f09be6482701e285752
indra.army <indra.army@skyshi.com>
2023-03-23 07:40:20 +0700

    feat(delegation): detail delegation

--- BigioHrServices/Services/DelegationServices.cs
+++ BigioHrServices/Services/DelegationServices.cs
@@ -13,6 +13,7 @@ namespace BigioHrServices.Services
     public interface IDelegationService
     {
         public DatatableResponse GetList(DelegationSearchRequest request);
+        public DelegationDetailResponse GetDetailDelegation(string nik);
         public void DelegationAdd(DelegationAddRequest request);
     }
 
@@ -39,13 +40,19 @@ namespace BigioHrServices.Services
             }
 
             var data = query
-                .Select(_delegation => new DelegationResponse
-                {
-                    NIK = _delegation.NIK,
-                    ParentNIK = _delegation.ParentNIK,
-                    Priority = _delegation.Priority
-                })
                 .GroupBy(x => new {x.ParentNIK})
+                .Join(_db.Employees, d => d.Key.ParentNIK, 
+                    e => e.NIK, (d, e) => new 
+                    {
+                        NIK = e.NIK,
+                        Name = e.Name,
+                        Count = d.Count()
+                    })
+                .Select(g => new {
+                    NIK = g.NIK,
+                    Name = g.Name,
+                    BackupCount = g.Count
+                })
                 .ToList();
 
             return new DatatableResponse()
@@ -58,13 +65,61 @@ namespace BigioHrServices.Services
             };
         }
 
+        public DelegationDetailResponse GetDetailDelegation(string nik)
+        {
+            var data = _db.Delegations
+                .Where(p => p.ParentNIK == nik)
+                .Select(_delegation => new DelegationResponse
+                {
+                    NIK = _delegation.NIK,
+                    ParentNIK = _delegation.ParentNIK,
+                    Priority = _delegation.Priority
+                })
+                .OrderBy(x => x.Priority)
+                .ToList();
+            
+            if (data.Count == 0) {
+                throw new Exception("NIK tidak ditemukan");
+            }
+
+            return new DelegationDetailResponse()
+            {
+                NIK = nik,
+                Backups = data.ToArray()
+            };
+        }
+
         public void DelegationAdd(DelegationAddRequest request)
         {
+            var dataEmployee = _db.Employees
+                .Where(p => p.NIK == request.ParentNIK)
+                .FirstOrDefault();
+            
+            if (dataEmployee == null) {
+                throw new Exception("NIK Pegawai "+request.ParentNIK+" tidak ditemukan.");
+            }
+
+            var dataEmployeeBackup = _db.Employees
+                .Where(p => p.NIK == request.NIK)
+                .FirstOrDefault();
+
+            if (dataEmployeeBackup == null) {
+                throw new Exception("NIK Backup "+request.NIK+" tidak ditemukan.");
+            }
+
             var data = _db.Delegations
                 .Where(p => p.ParentNIK == request.ParentNIK)
                 .AsNoTracking()
                 .ToList();
-            if (data.Count >= 3) throw new Exception("NIK "+request.ParentNIK+"sudah memiliki 3 backup");
+
+            if (data.Count >= 3) throw new Exception("NIK "+request.ParentNIK+" sudah memiliki 3 backup.");
+
+            var dataNIKBackup = _db.Delegations
+                .Where(p => p.NIK == request.NIK)
+                .AsNoTracking()
+                .ToList();
+
+            if (dataNIKBackup.Count >= 2) throw new Exception("NIK "+request.NIK+" sudah jadi 2 backup pegawai.");
 
             try
             {
@@ -73,7 +128,7 @@ namespace BigioHrServices.Services
                     NIK = request.NIK,
                     ParentNIK = request.ParentNIK,
                     Priority = request.Priority
-                }); ;
+                });
                 _db.SaveChanges();
             }
             catch (Exception ex)
